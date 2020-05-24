129fc8187b2c4125b10b9906b6fe690513964e86
George Matthew <george02@terpmail.umd.edu>
2020-05-24 17:48:54 -0400

    Added test map & player

---
+++ HorrorGame/Assets/MovingRoom.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class MovingRoom : MonoBehaviour
+{
+
+    [SerializeField] private GameObject targetLocation;
+    [SerializeField] private GameObject player;
+    [SerializeField] private GameObject currArea;
+    [SerializeField] private GameObject targetArea;
+
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        currArea.SetActive(false);
+        targetArea.SetActive(true);
+
+        player.transform.position = targetLocation.transform.position;
+    }
+
+}
