7ef094e665b735d014524f1af88f09776346d434
George Matthew <george02@terpmail.umd.edu>
2020-05-31 02:36:41 -0400

    Added player interaction function

---
+++ HorrorGame/Assets/Interactable.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class Interactable : MonoBehaviour
+{
+    [SerializeField] private float radius = 0.5f;
+
+    //visualize the radius where the player can interact with the item
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(transform.position, radius);
+    }
+}
