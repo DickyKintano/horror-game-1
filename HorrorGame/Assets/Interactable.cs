415e92df6569bda41c5eede02a74a85c72e170c7
George Matthew <george02@terpmail.umd.edu>
2020-05-31 03:45:38 -0400

    Added player interaction system

---
+++ HorrorGame/Assets/Interactable.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class Interactable : MonoBehaviour
+{
+    [SerializeField] private float radius = 0.5f;
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(transform.position, radius);
+    }
+}
+
